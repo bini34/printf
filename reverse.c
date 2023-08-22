7ec0365c008eb288dd20011435dfaee90f05d535
henok-yes <henokakalu1994@gmail.com>
2023-08-22 12:03:36 -0700

    a file that contain afunction that reverse sting is added

---
+++ reverse.c
@@ -0,0 +1,10 @@
+/**
+* print_reversed_String- Prints string reverswise
+* @strng: input string to be printed
+*/
+void print_reversed_String(const char *strng)
+{
+int length = strlen(strng);
+for (int x = length - 1; x >= 0; x--)
+printf("%c", stiirng[x]);
+}
