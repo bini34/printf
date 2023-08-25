26309516f8c53f06eae169e213a5a542263d8417
bini34 <biniyamambachew@gmail.com>
2023-08-25 21:00:41 +0300

    update task 6

--- print_pointer.c
+++ print_pointer.c
@@ -6,27 +6,42 @@
  */
 void print_pointer(va_list args, int *count)
 {
-	int i, index, num_digits = sizeof(void *) * 2, leading_zeros = 1;
-	unsigned long int ptr = (unsigned long int)va_arg(args, void *);
-	char hex_digits[] = "0123456789abcdef";
+	void *ptr = va_arg(args, void *);
+	char str[] = "(nil)";
+	int i = 0;
 
-	_putchar('0');
-	_putchar('x');
-    (*count) += 2;
-	for (i = num_digits - 1; i >= 0; i--)
+	if (ptr == NULL)
 	{
-		index = (ptr >> (i * 4)) & 0xf;
-		if (hex_digits[index] != '0' || !leading_zeros)
+		while(str[i] != '\0')
 		{
-			_putchar(hex_digits[index]);
-			(*count)++;
-			leading_zeros = 0;
+			_putchar(str[0]);
+			(*count) += 5;
+			i++;
 		}
-	}
-	if (leading_zeros)
+    }
+	else
 	{
+		unsigned long int num = (unsigned long int)ptr;
+		int index, num_digits = sizeof(void *) * 2, leading_zeros = 1;
+		char hex_digits[] = "0123456789abcdef";
 		_putchar('0');
-		(*count)++;
+		_putchar('x');
+		(*count) += 2;
+		for (i = num_digits - 1; i >= 0; i--)
+		{
+			index = (num >> (i * 4)) & 0xf;
+			if (hex_digits[index] != '0' || !leading_zeros)
+			{
+				_putchar(hex_digits[index]);
+				(*count)++;
+				leading_zeros = 0;
+			}
+		}
+		if (leading_zeros)
+		{
+			_putchar('0');
+			(*count)++;
+		}
 	}
 }
 
