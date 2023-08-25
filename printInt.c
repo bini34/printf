7e97c449e1c006a77d548068c1aad07f5e6b99a1
bini34 <biniyamambachew@gmail.com>
2023-08-25 20:44:14 +0300

    update task 1

--- printInt.c
+++ printInt.c
@@ -1,40 +1,44 @@
 #include "main.h"
-/**
- * print_positive_int - a helper function to print a positive integer
- * @n: the positive integer to print
- * @count: A pointer to the count of characters printed
- */
-void print_positive_int(int n, int *count)
-{
-	if (n == 0)
-		return;
-	print_positive_int(n / 10, count);
-	_putchar('0' + n % 10);
-	(*count)++;
-}
 
 /**
- * format_int - a function that handles %i and %d format specifiers
- * @args: va_list containing the integer argument
+ * format_int - a helper function to print an integer
+ * @n: the integer to print
  * @count: A pointer to the count of characters printed
  */
 void format_int(va_list args, int *count)
 {
-	int num = va_arg(args, int);
+	int n = va_arg(args, int), i = 1;
+	int num, last = n % 10, digit, exp = 1;
 
-	if (num == 0)
-	{
-		_putchar('0');
-		(*count)++;
-		return;
-	}
-
-	if (num < 0)
+	n = n / 10;
+	num = n;
+	if (last < 0)
 	{
 		_putchar('-');
-		(*count)++;
 		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+		(*count)++;
 	}
-	print_positive_int(num, count);
-}
 
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			(*count)++;
+			num = num - (digit * exp);
+			exp = exp / 10;
+		}
+	}
+	_putchar(last + '0');
+	(*count)++;
+}
